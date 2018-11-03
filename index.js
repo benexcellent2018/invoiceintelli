310000d63423ea6792943eaa3a0bfcd6207648a7
Henry Wu <>
2018-11-03 14:22:01 -0700

    Used Heroku example code at https://codeburst.io/node-js-on-heroku-a-more-complete-tutorial-part-1-9e80cb071498

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
 });
-app.listen(80, function () {
-  console.log('Example app listening on port 80!');
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
 
